3128b2c1e3fd191c2d3844e7da3b04cff8f6ec73
laiganc <33195580+laiganc@users.noreply.github.com>
2017-10-29 18:17:53 +0800

    Revert "Revert "Initial commit""
    This reverts commit e052d39bed84a30c0863067244019cede28a86ce.

---
+++ src/main/java/com/molin/MolinConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.molin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolinConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolinConfigClientApplication.class, args);
+	}
+}
